071f56a00b639fb6d6085a25ee23cf242b9e82a5
M-Zuber <mez613@gmail.com>
2014-02-19 21:26:59 +0200

    Added entity style mapping for Expense
    Also added mappings for dependant types

---
+++ DataAccess/Expense.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrameWork;
+
+namespace DataAccess
+{
+    public class ExpenseEntity
+    {
+        #region Properties
+
+        /// <summary>
+        /// The amount of the expense
+        /// </summary>
+        public double Amount { get; set; }
+
+        /// <summary>
+        /// The date of the expense
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Category of the expense
+        /// </summary>
+        public ExpenseCategoryEntity Category { get; set; }
+
+        /// <summary>
+        /// How the expense was payed
+        /// </summary>
+        public PaymentMethodEntity Method { get; set; }
+
+        /// <summary>
+        /// Additional info about the expense
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// ID number of the expense in the data table
+        /// </summary>
+        public uint ID { get; private set; }
+
+        #endregion
+
+        #region C'Tor
+
+        public ExpenseEntity(double amount, DateTime date, ExpenseCategoryEntity expenseCategory,
+            PaymentMethodEntity paymentMethod, string comment, uint id)
+        {
+            this.Amount = amount;
+            this.Category = expenseCategory;
+            this.Comment = comment;
+            this.Date = date;
+            this.ID = id;
+            this.Method = paymentMethod;
+        }
+
+        #endregion
+
+        #region CRUD Methods
+
+        #region Read Methods
+
+        public static ExpenseEntity LoadById(int id)
+        {
+            StaticDataSet.t_expensesRow requestedRow =
+                Cache.SDB.t_expenses.FindByID((uint)id);
+            return new ExpenseEntity(requestedRow.AMOUNT, requestedRow.EXP_DATE,
+                ExpenseCategoryEntity.LoadById(requestedRow.CATEGORY), 
+                PaymentMethodEntity.LoadById(requestedRow.METHOD), requestedRow.COMMENTS, requestedRow.ID);
+        }
+
+        public static List<ExpenseEntity> LoadAll()
+        {
+            List<ExpenseEntity> allExpensesCategories = new List<ExpenseEntity>();
+
+            foreach (StaticDataSet.t_expensesRow currExpense in Cache.SDB.t_expenses.Rows)
+            {
+                allExpensesCategories.Add(TranslateFromDataRow(currExpense));
+            }
+
+            return allExpensesCategories;
+        }
+
+        public static List<ExpenseEntity> LoadOfMonth(DateTime monthWanted)
+        {
+            List<ExpenseEntity> allExpensesCategories = new List<ExpenseEntity>();
+
+            foreach (StaticDataSet.t_expensesRow currExpense in Cache.SDB.t_expenses.Rows)
+            {
+                if ((currExpense.EXP_DATE.Month == monthWanted.Month) &&
+                    (currExpense.EXP_DATE.Year == monthWanted.Year))
+                {
+                    allExpensesCategories.Add(TranslateFromDataRow(currExpense)); 
+                }
+            }
+
+            return allExpensesCategories;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Other Methods
+
+        public static ExpenseEntity TranslateFromDataRow(StaticDataSet.t_expensesRow rowTranslating)
+        {
+            if (rowTranslating.IsCOMMENTSNull())
+            {
+                rowTranslating.COMMENTS = string.Empty;
+            }
+
+            return new ExpenseEntity(rowTranslating.AMOUNT, rowTranslating.EXP_DATE,
+                ExpenseCategoryEntity.LoadById(rowTranslating.CATEGORY),
+                PaymentMethodEntity.LoadById(rowTranslating.METHOD), rowTranslating.COMMENTS, rowTranslating.ID);
+        }
+
+        #endregion
+    }
+}
---
+++ DataAccess/PaymentMethod.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrameWork;
+
+namespace DataAccess
+{
+    public class PaymentMethodEntity
+    {
+        #region Properties
+
+        /// <summary>
+        /// The name (type) of the payment method category
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Id number of the payment method category in the table
+        /// </summary>
+        public int ID { get; internal set; }
+
+        #endregion
+
+        #region C'Tor
+
+        public PaymentMethodEntity(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+
+        #endregion
+
+        #region CRUD Methods
+
+        #region Read Methods
+
+        public static PaymentMethodEntity LoadById(int id)
+        {
+            StaticDataSet.t_payment_methodsRow requestedRow =
+                Cache.SDB.t_payment_methods.FindByID(id);
+            return new PaymentMethodEntity(requestedRow.ID, requestedRow.NAME);
+        }
+
+        public static List<PaymentMethodEntity> LoadAll()
+        {
+            List<PaymentMethodEntity> allPaymentMethods = new List<PaymentMethodEntity>();
+
+            foreach (StaticDataSet.t_payment_methodsRow currPaymentMethod in Cache.SDB.t_payment_methods.Rows)
+            {
+                allPaymentMethods.Add(
+                    new PaymentMethodEntity(currPaymentMethod.ID, currPaymentMethod.NAME));
+            }
+
+            return allPaymentMethods;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Other Methods
+
+        #region Override Methods
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
