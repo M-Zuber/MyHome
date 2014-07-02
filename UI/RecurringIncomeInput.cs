3b2c4ab73f254e23f268798aae312d0a6940a644
Mordechai Zuber <mez613@gmail.com>
2014-07-02 22:31:31 +0300

    Removed old bl from RecurringIncome form
    Complies with #29

--- UI/RecurringIncomeInput.cs
+++ UI/RecurringIncomeInput.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
-using BL;
-using Old_FrameWork;
+using LocalTypes;
+using BusinessLogic;
 
 namespace MyHome2013
 {
@@ -37,13 +37,13 @@ namespace MyHome2013
         {
             // Sets up the combo box of the income categories
             this.cmbCategory.DataSource =
-                Cache.SDB.t_incomes_category;
+                (new IncomeCategoryHandler()).LoadAll();
             this.cmbCategory.DisplayMember = "NAME";
             this.cmbCategory.ValueMember = "ID";
 
             // Sets up the combo box with the payment methods
             this.cmbPayment.DataSource =
-                Cache.SDB.t_payment_methods;
+                (new PaymentMethodHandler()).LoadAll();
             this.cmbPayment.DisplayMember = "NAME";
             this.cmbPayment.ValueMember = "ID";
 
@@ -71,7 +71,7 @@ namespace MyHome2013
                                 MessageBoxDefaultButton.Button1);
             }
             // Checks that the amount is in numbers
-            else if (!GlobalBL.IsNumeric(this.txtAmount.Text))
+            else if (!HelperMethods.IsNumeric(this.txtAmount.Text))
             {
                 MessageBox.Show("The amount must be in numbers",
                                 "Error",
@@ -212,22 +212,20 @@ namespace MyHome2013
             // Loops for the amount of days in the range
             for (int nDayIndex = 0; nDayIndex < nDaysRange; nDayIndex++)
             {
-                // Creates a new income and sets the fields accordingly
-                IncBL incNewInc = IncBL.CreateIncome();
-                incNewInc.Amount = this.txtAmount.Text;
-                incNewInc.Date = dtCurrentSaveDate;
-                incNewInc.Category =
-                    Convert.ToInt32(this.cmbCategory.SelectedValue);
-                incNewInc.Method =
-                    Convert.ToInt32(this.cmbPayment.SelectedValue);
-                incNewInc.Comment = this.txtDetail.Text;
-
-                // Saves the new income into the cache
-                incNewInc.Save();
+                CreateNewIncome(dtCurrentSaveDate);
 
                 // Ups the date for the next expense
                 dtCurrentSaveDate = dtCurrentSaveDate.AddDays(1);
             }
+        }
+
+        private void CreateNewIncome(DateTime dtCurrentSaveDate)
+        {
+            Income newIncome =
+                    new Income(double.Parse(this.txtAmount.Text), dtCurrentSaveDate,
+                                IncomeCategoryHandler.LoadById(Convert.ToInt32(this.cmbCategory.SelectedValue)),
+                                PaymentMethodHandler.LoadById(Convert.ToInt32(this.cmbPayment.SelectedValue)),
+                                this.txtDetail.Text);
         }
 
         /// <summary>
@@ -290,18 +288,7 @@ namespace MyHome2013
                 // Loops for the amount of months in the range
                 for (int nMonthIndex = 0; nMonthIndex < nMonthsRange; nMonthIndex++)
                 {
-                    // Creates a new income and sets the fields accordingly
-                    IncBL incNewInc = IncBL.CreateIncome();
-                    incNewInc.Amount = this.txtAmount.Text;
-                    incNewInc.Date = dtCurrentSaveDate;
-                    incNewInc.Category =
-                        Convert.ToInt32(this.cmbCategory.SelectedValue);
-                    incNewInc.Method =
-                        Convert.ToInt32(this.cmbPayment.SelectedValue);
-                    incNewInc.Comment = this.txtDetail.Text;
-
-                    // Saves the new income into the cache
-                    incNewInc.Save();
+                    CreateNewIncome(dtCurrentSaveDate);
 
                     // Ups the date for the next income
                     // If the new month has less days than it will automatically set the day 
@@ -326,18 +313,7 @@ namespace MyHome2013
             // Loops for the amount of years in the range
             for (int nYearIndex = 0; nYearIndex < nYearsInRange; nYearIndex++)
             {
-                // Creates a new income and sets the fields accordingly
-                IncBL incNewInc = IncBL.CreateIncome();
-                incNewInc.Amount = this.txtAmount.Text;
-                incNewInc.Date = dtCurrentSaveDate;
-                incNewInc.Category =
-                    Convert.ToInt32(this.cmbCategory.SelectedValue);
-                incNewInc.Method =
-                    Convert.ToInt32(this.cmbPayment.SelectedValue);
-                incNewInc.Comment = this.txtDetail.Text;
-
-                // Saves the new income into the cache
-                incNewInc.Save();
+                CreateNewIncome(dtCurrentSaveDate);
 
                 // Ups the date for the next expense
                 dtCurrentSaveDate = dtCurrentSaveDate.AddYears(1);
