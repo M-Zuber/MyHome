a99ac4504ce60fbe8cb6952c723cc749b9bdf148
Mordechai Zuber <mez613@gmail.com>
2015-03-31 01:56:23 +0300

    setup and initial work for feature tests

---
+++ MyHome.Spec/CategoryManagement.feature
@@ -0,0 +1,15 @@
+﻿Feature: CategoryManagement
+	As a user I want to be able to manage the 
+	categories for the flow items
+
+Scenario: Adding a category with an unique name
+	Given I have entered "food" as the name of the category
+	And there is no other category with that name
+	When I press add
+	Then the category should be added to the list
+
+Scenario: Adding a category with a duplicate name
+	Given I have entered "food" as the name of the category
+	And there is another category with the same name
+	When I press add
+	Then the category is not added to the list
---
+++ MyHome.Spec/Steps/CategoryManagementSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MyHome.Spec
+{
+    [Binding]
+    public class CategoryManagementSteps
+    {
+        [Given(@"I have entered ""(.*)"" as the name of the category")]
+        public void GivenIHaveEnteredAsTheNameOfTheCategory(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"there is no other category with that name")]
+        public void GivenThereIsNoOtherCategoryWithThatName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"there is another category with the same name")]
+        public void GivenThereIsAnotherCategoryWithTheSameName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the category should be added to the list")]
+        public void ThenTheCategoryShouldBeAddedToTheList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the category is not added to the list")]
+        public void ThenTheCategoryIsNotAddedToTheList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
