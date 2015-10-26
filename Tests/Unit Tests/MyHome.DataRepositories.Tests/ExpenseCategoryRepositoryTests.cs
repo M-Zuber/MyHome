005e06c790d2278fd0815f5a884cf6a33652153f
Mordechai Zuber <mez613@gmail.com>
2015-10-26 20:44:06 +0200

    init commit of test project for data repositories

---
+++ Tests/Unit Tests/MyHome.DataRepositories.Tests/ExpenseCategoryRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyHome.DataRepository;
+
+namespace MyHome.DataRepositories.Tests
+{
+    [TestClass]
+    public class ExpenseCategoryRepositoryTests
+    {
+        private Mock<ExpenseCategoryRepository> GetMock()
+        {
+            return new Mock<ExpenseCategoryRepository>();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
